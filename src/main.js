d8eb03403a40388cf22258d5db22385ae3d7d4e3
Shivam Mishra <scm.mymail@gmail.com>
2024-01-16 16:41:12 +0530

    feat: better logs

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import "./style.css";
 import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
@@ -15,17 +13,32 @@ document.querySelector("#app").innerHTML = `
   </div>
 `;
 
+export function setupCounter(element) {
+  let counter = 0;
+  const setCounter = (count) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+    window.parent.postMessage("chatwoot-dashboard-app:fetch-info", "*");
+    logEverywhere("SET COUNTER");
+  };
+  element.addEventListener("click", () => setCounter(counter + 1));
+  setCounter(0);
+}
+
 // Log to the console and to the #log element
 // @ts-ignore
 window.logEverywhere = (...args) => {
   console.log(...args);
   const log = document.querySelector("#log");
-  log.innerHTML += `<div>${args.join(" ")}</div>`;
+  const currentTimestamp = new Date().toLocaleTimeString();
+  log.innerHTML += `<div><span class="muted">${currentTimestamp}</span> ${args.join(
+    " "
+  )}</div>`;
   log.scrollTop = log.scrollHeight;
 };
 
 const setDataFromMessage = (event) => {
-  logEverywhere(event);
+  logEverywhere(event.data);
 };
 
 setupCounter(document.querySelector("#counter"));
